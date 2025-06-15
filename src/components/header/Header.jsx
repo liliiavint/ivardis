fbf6484547abcc632fcd7c1ac6835f6233771502
liliia <rudapanda2016@gmail.com>
2025-06-15 17:30:03 +0300

    css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation} from 'react-router-dom';
 import style from './Header.module.css';
 import { SearchBar } from '../search/SearchBar';
 import { useState } from 'react';
@@ -6,16 +6,26 @@ import { Logo } from '../logo/logo';
 
 export function Header() {
     const [searchOpen] = useState(true);
+    const location = useLocation();
+    const routeTitles = {
+      '/jus': 'Jūs - Ви',
+      '/mes': 'Mes - Ми',
+      '/jie': 'Jie - Вони',
+      '/jos': 'Jos - Вони'
+    };
+    const title = routeTitles[location.pathname];
 
    return  (
     <header className={`${style.header} ${style.container} ${searchOpen ? style.headerStandart : style.headerSearch}`} >
             <div className={searchOpen ? style.logoBox : style.logoBoxClose}>
                 <Link className={style.headerLogo} to="/"><Logo/></Link>
+                {title && <h1 className={style.circle}>{title}</h1>}
             </div>
+           
             <div className={searchOpen ? style.searchBox : style.searchBoxClose}> 
                 <SearchBar />
             </div>
-            
+          
     </header>
     );
 }
--- src/components/jus/Jus.jsx
+++ src/components/jus/Jus.jsx
@@ -3,11 +3,50 @@ import style from './Jus.module.css';
 export function Jus() {
     return (
         <main className={style.homeJus}>
-            <div className={`${style.circle} ${style.jusu}`}>Jūsų</div>
-        <div className={`${style.circle} ${style.jums}`}>Jums</div>
-        <div className={`${style.circle} ${style.jus}`}>Jus</div>
-        <div className={`${style.circle} ${style.jumis}`}>Jumis</div>
-        <div className={`${style.circle} ${style.jumyse}`}>Jumyse</div>
+                <div className={ style.jusu}>
+                    <div className={style.head}>
+                        <h2> Kilmininkas. Ko? Jūsų</h2>
+                    </div>
+                    <div className={style.groupblock}>
+                        <div className={style.block}>
+                            <h3 className={style.text}>Приналежність</h3>
+                            <p className={style.text1}>Jūsų knyga – labai įdomi. - Ваша книга дуже цікава. </p>
+                            <p className={style.text1}>Jūsų automobilis laukia lauke. — Ваш автомобіль чекає на вулиці.</p>
+                        </div>
+                        <div className={style.block}>  
+                            <h3 className={style.text}>Об'єкт дії, напрямок</h3>
+                            <p className={style.text1}>Aš ieškau jūsų.- Я шукаю вас.</p>
+                            <p className={style.text1}>Jūsų bankas - arčiau jūsų. - Ваш банк - ближче до Вас</p>
+                            <p className={style.text1}>Mes laukiame jūsų. — Ми чекаємо на вас.</p>
+                            <p className={style.text1}>Arčiau jūsų — Ближче до вас.</p>
+                        </div>
+                        <div className={style.block}> 
+                            <h3 className={style.text}>Причина, джерело</h3>
+                            <p className={style.text1}>"Dėl jūsų" - Через вас / Завдяки вам.</p>
+                        </div>
+                        <div className={style.block}>
+                            <h3 className={style.text}>Об'єкт бажання, потреби або думки</h3>
+                            <p className={style.text1}>"Man reikia jūsų patarimo" - Мені потрібна ваша порада.</p>
+                            <p className={style.text1}>Jūsų nuomonė mums svarbi. - Ваша думка важлива для нас.</p>
+                        </div>
+                    </div>
+                </div>
+                 <div className={ style.jums}>
+                    <div className={style.head}>
+                        <h2> Naudininkas. Kam? Jums</h2>
+                    </div>
+                    <div className={style.groupblock}>
+                        <div className={`${style.block1} ${style.block}`}>
+                            <h3 className={style.text}>Позначає адресата, особу, на яку спрямована дія або якій вона вигідна.</h3>
+                            <p className={style.text1}>Tai yra jums. – Це вам. </p>
+                            <p className={style.text1}>Padėka jums už pagalbą. – Подяка вам за допомогу.</p>
+                            <p className={style.text1}>Aš jums daviau pavyzdį. Я навів тобі приклад</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={`${style.circle} ${style.jus}`}>Jus</div>
+                <div className={`${style.circle} ${style.jumis}`}>Jumis</div>
+                <div className={`${style.circle} ${style.jumyse}`}>Jumyse</div>
         </main>
     )
 }
